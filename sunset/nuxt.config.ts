7e2937f729eea93dfe921c6d9c95b9b06057db23
Juanjo Salvador <juanjosalvador@netc.eu>
2024-10-23 10:19:34 +0200

    Initial commit

---
+++ sunset/nuxt.config.ts
@@ -0,0 +1,18 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  css: ['@/node_modules/bulma/css/bulma.min.css'],
+  modules: [
+    '@pinia/nuxt',
+    '@vesp/nuxt-fontawesome'
+  ],
+  plugins: [
+    { src: '~/plugins/bsky-agent.ts', mode: 'client' },
+  ],
+  fontawesome: {
+    icons: {
+      solid: ['repeat', 'heart', 'bell', 'right-from-bracket', 'house', 'reply', 'book-open'],
+      regular: ['comment', 'heart']
+    }
+  }
+})
