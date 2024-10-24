ba90e42f786779722e96f84962580787954c853e
Juanjo Salvador <juanjosalvador@netc.eu>
2024-10-24 23:54:23 +0200

    fix(post): breaklines and wrapping is respected

--- src/components/post/PostText.vue
+++ src/components/post/PostText.vue
@@ -24,7 +24,7 @@ const { htmlText } = useRichText(props.text, props.agent)
 <style scoped>
 div.postText {
   margin-left: 4rem;
-  font-size: normal;
+  white-space: pre-wrap;
 }
 
 div.postText > p + p {
