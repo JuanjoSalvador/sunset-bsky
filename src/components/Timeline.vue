9a0507452328b45aae25da4eba891bc2e955f1e2
Juanjo Salvador <juanjosalvador@netc.eu>
2024-11-01 14:55:44 +0100

    fix(timeline): fix integration with new service

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -3,13 +3,12 @@ import { useSessionStore } from '~/stores/session'
 import Post from './post/Post.vue';
 
 const nuxtApp = useNuxtApp()
-const bskyAgent = nuxtApp.$bskyAgent
+const bskyAgent = nuxtApp.$agent
 const sessionStore = useSessionStore()
 
 const loading = ref(false)
 const timelineData = ref<Array<any>>([])
-const cursor = ref<string | null>(null)
-const endReached = ref(false)
+const cursor = ref<string | undefined>(undefined)
 const observer = ref<IntersectionObserver | null>(null)
 
 const savedSessionData = sessionStore.getSession()
@@ -19,45 +18,31 @@ async function resumeSession() {
     await bskyAgent.resumeSession(savedSessionData)
 }
 
-async function fetchData(cursorValue?: string | null) {
+async function fetchData(cursorValue?: string | '') {
+  loading.value = true
+
   try {
-    loading.value = true
+    let options = cursorValue === null
+      ? {'limit': 20}
+      : {'limit': 20, 'cursor': cursorValue}
 
-    console.log("Updating data from Bsky")
+    const response = await useTimeline(options)
 
-    const response = cursorValue === null
-      ? await bskyAgent.getTimeline({ limit: 50 })
-      : await bskyAgent.getTimeline({ cursor: cursorValue, limit: 50 })
-
-    // Skip replies and reposts from current user
-    const filteredPosts = response.data.feed.filter(
+    const filteredPosts = response?.feed.filter(
       post => !(post.post.record as any).reply || !(post.reason?.by?.did == savedSessionData?.did)
     )
 
-    timelineData.value = [...timelineData.value, ...filteredPosts]
-
-    if (response.data.cursor) {
-      cursor.value = response.data.cursor
-      endReached.value = false // There's more data to load
+    timelineData.value = [...timelineData.value, ...(filteredPosts || [])]
+    
+    if (response?.cursor) {
+      cursor.value = response?.cursor
     } else {
-      endReached.value = true // We've reached the end
       if (observer.value)
         observer.value.unobserve(document.querySelector('#endOfList')!)
     }
-  }
-
-  catch (error: unknown) {
-    if (error instanceof Error) {
-      console.error('Failed:', error.message)
-      if (error.message.includes('Authentication Required'))
-        sessionStore.clearSession()
-    }
-    else {
-      console.error('An unexpected error occurred:', error)
-    }
-  }
-
-  finally {
+  } catch (error) {
+    console.log("An error ocurred!", error)
+  } finally {
     loading.value = false
   }
 }
@@ -65,26 +50,26 @@ async function fetchData(cursorValue?: string | null) {
 onBeforeMount(() => {
   resumeSession()
   fetchData()
-})
+}) 
 
 onMounted(() => {
   // Infinite scroll, loads new posts when last one has been reached
   observer.value = new IntersectionObserver(async (entries) => {
   if (entries[0].isIntersecting && !loading.value)
-    await fetchData(cursor.value)
+    fetchData(cursor.value)
   }, {
-      rootMargin: '200px 0px',
+      rootMargin: '100px 0px',
   })
 
   observer.value.observe(document.querySelector('#endOfList')!)
-})
-
+}) 
 </script>
 
 <template>
   <ul>
-    <Post v-for="post in timelineData" :key="post.post.record.cid" :post="post" :agent="bskyAgent" />
+    <Post v-for="post in timelineData" :post="post" :agent="bskyAgent"/>
   </ul>
+  
   <div id="endOfList" />
 </template>
 
