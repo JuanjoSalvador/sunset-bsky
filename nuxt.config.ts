95e9683c01e8c4ee8a9371a24f17a6460c7b4a32
Juanjo Salvador <juanjosalvador@netc.eu>
2024-11-01 14:24:50 +0100

    fix(content): better emoji support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    keepalive: true
+  },
   css: ['bulma/css/bulma.min.css'],
-  modules: [
-    '@pinia/nuxt',
-    '@vesp/nuxt-fontawesome'
-  ],
+  modules: ['@pinia/nuxt', '@vesp/nuxt-fontawesome', 'nuxt-twemoji', '@nuxtjs/google-fonts'],
   plugins: [
     { src: '~/plugins/bsky-agent.ts', mode: 'client' },
   ],
   fontawesome: {
     icons: {
-      solid: ['repeat', 'heart', 'bell', 'right-from-bracket', 'house', 'reply', 'book-open'],
+      solid: ['repeat', 'heart', 'bell', 'right-from-bracket', 'house', 'reply', 'book-open', 'share-nodes'],
       regular: ['comment', 'heart']
     }
   },
