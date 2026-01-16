772ba026f391f1762eb722fb14b1dc4000cde841
Juanjo Salvador <juanjosalvador@netc.eu>
2026-01-16 18:35:47 +0100

    (components) feat: add Announcement component

---
+++ src/components/Announcement.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="notification">
+        <h2 class="title is-4 has-text-centered">
+            Disclaimer
+        </h2>
+        <p>
+            It may contain bugs and is not affiliated with Bluesky. Use at your own risk. Not everything may work as expected, and features may change frequently.
+        </p>
+        <br/>
+        <p>
+            <strong>If any error found, please report on <a href="https://github.com/JuanjoSalvador/sunset-bsky/issues">GitHub</a>.</strong>
+        </p>
+    </div>
+</template>
+
+<style scoped>
+</style>
